e7a35e58accba0ce4345ed1a46621fafcd522d7d
Jake Bills <jakebills1@gmail.com>
2019-08-23 11:38:03 -0600

    model structure set up

--- db/migrate/20190823171108_add_playlist_to_charts.rb
+++ db/migrate/20190823171108_add_playlist_to_charts.rb
@@ -1,5 +1,5 @@
 class AddPlaylistToCharts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :charts, :playlist, foreign_key: true
+    add_reference :charts, :playlist, foreign_key: true, optional: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_170914) do
+ActiveRecord::Schema.define(version: 2019_08_23_171108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_08_23_170914) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "playlist_id"
+    t.index ["playlist_id"], name: "index_charts_on_playlist_id"
     t.index ["user_id"], name: "index_charts_on_user_id"
   end
 
@@ -64,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_08_23_170914) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "charts", "playlists"
   add_foreign_key "charts", "users"
   add_foreign_key "playlists", "users"
 end
